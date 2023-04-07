7cfa235b8ab1bf127ee7fa4cf208bffac6575e39
Arturo <arturotinajas@gmail.com>
2023-04-07 23:59:11 +0200

    fix

--- src/database/Record.js
+++ src/database/Record.js
@@ -2,11 +2,10 @@ const DB = require('./db.json')
 
 const getRecordForWorkout = (workoutId) => {
   try {
-    const record = DB.filter(workout => workout.id === workoutId)
-    console.log("record", record)
-    if (!record)
+    const record = DB.records.filter(record => record.workout == workoutId)
+    if (!record.length)
       throw {
-        status: error?.status || 400,
+        status: 400,
         message: `Cant't find workout with Id '${workoutId}'`
       }
     return record;
